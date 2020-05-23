dc4071ee55b0674d5a5d0720e139e504f9e9f83c
vfsettle <56850679+vfsettle@users.noreply.github.com>
2020-05-23 07:12:14 -0700

    Change to port 3000

--- helloworld.js
+++ helloworld.js
@@ -8,8 +8,8 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Welcome Message"
-   response.end(' First Web App Deployment by Vanessa Settle\n')
-}).listen(3333)
+   response.end(' First Web App Deployment with Ansible Playbook by Vanessa Settle on 5.23.2020\n')
+}).listen(3000)
 
 // Console will print the message
 console.log('Server running')
